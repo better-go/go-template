5a9a50fac1cc25acfdc97a8d2dfd95dd287482b1
hhstore <hhglory@outlook.com>
2020-06-02 11:32:36 +0800

    init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,6 +8,22 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def choice():
+    # 选择:
+    if '{{ cookiecutter.folder_type }}' == 'Single Micro Service: App Folder':
+        single_app_create()  # 创建 单个 app 目录
+    else:
+        mono_repo_create()  # 创建 mono repo 根目录
+
+
+def mono_repo_create():
+    print("single app create:")
+
+
+def single_app_create():
+    print("single app create:")
+
+
 if __name__ == '__main__':
 
     if '{{ cookiecutter.create_author_file }}' != 'y':
