95a8fc0334f5d244b683fc15365bb45536b189cb
hhstore <hhglory@outlook.com>
2020-06-02 11:48:49 +0800

    init

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,13 +1,21 @@
 import re
 import sys
 
-
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
 module_name = '{{ cookiecutter.project_slug}}'
 
-if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: The project slug (%s) is not a valid Python module name. Please do not use a - and use _ instead' % module_name)
 
-    #Exit to cancel project
-    sys.exit(1)
+def validate_name(regex, name):
+    if not re.match(regex, name):
+        print(
+            'ERROR: The project slug (%s) is not a valid Python module name. Please do not use a - and use _ instead' % module_name)
+        return false
+    return true
+
+
+if __name__ == '__main__':
+    # validate:
+    if not validate_name(MODULE_REGEX, module_name):
+        # Exit to cancel project
+        sys.exit(1)
